44c3958a99a99758ada5cfdc10202a682be0af89
kitajchuk <bk@kitajchuk.com>
2021-03-14 17:14:12 -0700

    Adding static about page

--- components/navi.js
+++ components/navi.js
@@ -3,19 +3,18 @@ import { useRouter } from 'next/router';
 
 const Navi = () => {
   const router = useRouter();
+  const isAbout = (router.route === '/about');
   const isBookmarks = (router.route === '/bookmarks');
 
   return (
     <nav className="text-center font-sans text-sm sm:text-base font-light px-5 py-5">
-      {isBookmarks ? (
-        <Link href="/">
-          <a className="underline" title="Home">Home</a>
-        </Link>
-      ) : (
-        <Link href="/bookmarks">
-          <a className="underline" title="Bookmarks">Bookmarks</a>
-        </Link>
-      )}
+      <Link href="/bookmarks">
+        <a className={isBookmarks ? 'underline' : ''} title="Bookmarks">Bookmarks</a>
+      </Link>
+      <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+      <Link href="/about">
+        <a className={isAbout ? 'underline' : ''} title="About">About</a>
+      </Link>
     </nav>
   );
 };
