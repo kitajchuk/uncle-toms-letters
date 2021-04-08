672f708e8b42de5b10f70af0183224e5a0018a5e
kitajchuk <bk@kitajchuk.com>
2021-04-08 09:10:20 -0700

    Add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Layout from '../components/layout';
+import Masthead from '../components/masthead';
+import Linkback from '../components/linkback';
+
+const Page404 = () => {
+  return (
+    <Layout title="Not Found">
+      <Masthead title="Oops" subtitle="This page doesn't exist" />
+      <div className="flex justify-center pb-20 sm:pb-36">
+        <Linkback />
+      </div>
+    </Layout>
+  )
+};
+
+export default Page404;
