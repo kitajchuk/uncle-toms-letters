97433f5d84794a07d265a51b571cca2ac7bf63d6
kitajchuk <bk@kitajchuk.com>
2021-09-04 10:30:41 -0700

    Add start to masthead

--- components/masthead.js
+++ components/masthead.js
@@ -1,8 +1,14 @@
+import Star from './star';
 import { withAnimate } from './animate';
 
-const Masthead = ({title, subtitle}) => {
+const Masthead = ({title, subtitle, recent = false}) => {
   return (
     <header className="text-center font-sans py-20 sm:py-36 px-5 leading-loose">
+      {recent ? (
+        <div className="mb-5">
+          <Star />
+        </div>
+      ) : null}
       <h1 className="text-3xl sm:text-4xl font-normal">{title}</h1>
       <h2 className="text-xl sm:text-2xl mt-5 font-light">{subtitle}</h2>
     </header>
