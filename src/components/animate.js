80bf009449775f6f316de89467239f39399f55aa
kitajchuk <bk@kitajchuk.com>
2022-05-19 13:04:03 -0700

    intersection observer

--- src/components/animate.js
+++ src/components/animate.js
@@ -1,13 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 
-// HOC
-// Usage: export default withAnimate(SomeComponent);
-// https://reactjs.org/docs/higher-order-components.html
 export function withAnimate(WrappedComponent) {
   return function WrapperComponent({...props}) {
     const elRef = useRef();
-    const scRef = useRef();
+    const obRef = useRef();
     const [animated, setAnimated] = useState(false);
     const classes = {
       'anim': true,
@@ -16,30 +13,22 @@ export function withAnimate(WrappedComponent) {
 
     useEffect(() => {
       if (elRef.current) {
-        import('properjs-scrollcontroller').then((ScrollController) => {
-          scRef.current = new ScrollController.default();
-
-          scRef.current.on('scroll', () => {
-            if (elRef.current) {
-              const bounds = elRef.current.getBoundingClientRect();
-
-              if (bounds.top < window.innerHeight && bounds.bottom > 0) {
-                scRef.current.stop();
-                scRef.current.off('scroll');
-                setAnimated(true);
-              }
-            }
-          });
+        obRef.current = new IntersectionObserver((entries) => {
+          if (entries[ 0 ].isIntersecting) {
+            obRef.current.disconnect();
+            setAnimated(true);
+          }
         });
+
+        obRef.current.observe(elRef.current);
       }
 
       return function cleanup() {
-        if (scRef.current) {
-          scRef.current.stop();
-          scRef.current.off('scroll');
+        if (obRef.current) {
+          obRef.current.disconnect();
         }
       };
-    }, [elRef, scRef]);
+    }, []);
 
     return (
       <div ref={elRef} className={classNames(classes)}>
